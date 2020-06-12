d50425800b53c67fe72d2dcbb845daee7ed11b8d
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-06-12 15:39:25 -0300

    test(api): Added test coverage

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/static/',
+    '/database/',
+    '/test/',
+    'ormconfig.js',
+  ],
 };
