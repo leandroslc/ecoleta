14908b7bc9a06caeceb82d80962e16ead2b5ff60
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-06-12 00:06:16 -0300

    feat(api): Added routes validation

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 app.use('/static', express.static(path.resolve(__dirname, '..', 'static')));
+app.use(errors());
 
 export default app;
--- packages/api/src/routes/points.ts
+++ packages/api/src/routes/points.ts
@@ -1,14 +1,45 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 import multerConfig from '../config/multer';
 import { CollectionPointsController } from '../controllers';
+
+const CommaSeparatedNumbersRegex = /^[\d\\,\s]+$/;
+const PhoneNumberRegex = /^[\w\-+.\s\\(\\)\w]+$/;
 
 const routes = Router();
 const upload = multer(multerConfig);
 const pointsController = new CollectionPointsController();
 
+const validation = {
+  show: celebrate({
+    query: Joi.object().keys({
+      city: Joi.string(),
+      state: Joi.string(),
+      items: Joi.string().regex(CommaSeparatedNumbersRegex),
+    }),
+  }),
+  create: celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required().regex(PhoneNumberRegex),
+      city: Joi.string().required(),
+      state: Joi.string().required().max(2),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      items: Joi.string().regex(CommaSeparatedNumbersRegex),
+    }),
+  }),
+};
+
 routes.get('/', pointsController.index);
-routes.get('/:id', pointsController.show);
-routes.post('/', upload.single('image'), pointsController.create);
+routes.get('/:id', validation.show, pointsController.show);
+routes.post(
+  '/',
+  upload.single('image'),
+  validation.create,
+  pointsController.create,
+);
 
 export default routes;
