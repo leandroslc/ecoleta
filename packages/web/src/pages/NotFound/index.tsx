4b0e1c5e88d80017adeff522b448d22bbae00d0f
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-07-02 18:38:13 -0300

    feat(web): Added a 404 page

---
+++ packages/web/src/pages/NotFound/index.tsx
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Link as RouteLink } from 'react-router-dom';
+import { Header, Layout, Link, PageContent } from '../../components';
+import styles from './styles';
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <Header />
+      <PageContent>
+        <section css={styles.container}>
+          <h1 css={styles.title}>
+            Página não encontrada <span aria-hidden="true">😕</span>
+          </h1>
+          <Link component={RouteLink} to="/">
+            Voltar para o início
+          </Link>
+        </section>
+      </PageContent>
+    </Layout>
+  );
+};
+
+export default NotFound;
---
+++ packages/web/src/pages/NotFound/styles.ts
@@ -0,0 +1,20 @@
+import { css } from '@emotion/core';
+
+export const container = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 48px;
+`;
+
+export const title = css`
+  margin-bottom: 12px;
+  font-size: 26px;
+`;
+
+export default {
+  container,
+  title,
+};
